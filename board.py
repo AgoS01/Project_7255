666389aabbdf1033ee26918856a327c6c520b6fd
AgoS01 <tolyanchekaif@gmail.com>
2023-02-09 20:28:12 +0300

    Программа разбита на части, исправлено несколько багов

---
+++ board.py
@@ -0,0 +1,37 @@
+from sprites import Sp
+
+
+class Board:
+    # создание поля
+    def __init__(self, width, height, left=10, top=10, cell_size=30):
+        self.width = width
+        self.height = height
+        self.board = [[0] * width for _ in range(height)]
+        # значения по умолчанию
+        self.left = 10
+        self.top = 10
+        self.cell_size = 30
+
+    # настройка внешнего вида
+    def set_view(self, left, top, cell_size):
+        self.left = left
+        self.top = top
+        self.cell_size = cell_size
+
+    def render(self, screen):
+        pass
+
+    def on_click(self, mouse_pos):
+        pass
+
+    def get_cell(self, mouse_pos):
+        cell_x = (mouse_pos[0] - self.left) // self.cell_size
+        cell_y = (mouse_pos[1] - self.top) // self.cell_size
+        if cell_x < 0 or cell_x >= self.width or cell_y < 0 or cell_y >= self.height:
+            return None
+        return cell_x, cell_y
+
+    def get_click(self, mouse_pos):
+        cell = self.get_cell(mouse_pos)
+        if cell:
+            self.on_click(cell)
