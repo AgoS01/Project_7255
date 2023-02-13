165d96bc29ee278a362726503ce02ef07a5d7a0c
HeroBroN <66018244+HeroBroN@users.noreply.github.com>
2023-02-13 16:19:49 +0300

    Add files via upload

--- board.py
+++ board.py
@@ -1,4 +1,48 @@
 from sprites import Sp
+import pygame
+import sys
+# from menu_buttons import main_menu
+# from buttons_menu import Btn
+
+
+class victory:
+    pygame.init()
+
+    def gr_win():
+        img = pygame.image.load('data/green_won.png')
+        screen = pygame.display.set_mode((1280, 800))
+        screen.blit(img, (330, 20))
+        menu_ms_pos = pygame.mouse.get_pos()
+        menu_back = Btn(img=None, pos=(640, 460), txt_inp='Вернуться в меню',
+                        font=Font.new_font(75), clr_base='#9933ff',
+                        clr_hvr='#8000ff')
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if menu_back.checkForInput(menu_ms_pos):
+                    main_menu.options()
+
+        pygame.display.update()
+
+    def rd_win():
+        img = pygame.image.load('data/red_won.png')
+        screen = pygame.display.set_mode((1280, 800))
+        screen.blit(img, (330, 20))
+        menu_ms_pos = pygame.mouse.get_pos()
+        menu_back = Btn(img=None, pos=(640, 460), txt_inp='Вернуться в меню',
+                        font=Font.new_font(75), clr_base='#9933ff',
+                        clr_hvr='#8000ff')
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if menu_back.checkForInput(menu_ms_pos):
+                    main_menu.options()
+
+        pygame.display.update()
 
 
 class Board:
--- main.py
+++ main.py
@@ -1,89 +1,85 @@
-import os
-import sys
-import pygame
-import csv
-import copy
 from sandbox import*
 from main_board import MainBoard
-from sprites import Sp
 
 
-def main():
-    pygame.init()
-    mapp = 'map1.csv'
-    pygame.display.set_caption('Игра')
-    size = width, height = 1920 * 3 / 4, 1080 * 3 / 4
-    screen = pygame.display.set_mode(size)
-    board = MainBoard(11, 11, 100, 10, 70, mapp)
-    board.set_view(300, 10, 70)
-    board.first_render(screen)
-    os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
-    running = True
-    time_on = False
-    speed = 15
-    ticks = 0
-    last_chosen = None
-    default_mc = 3 # кол-во ходов по условию
-    move_counter = default_mc
-    board.player = 0
-    clock = pygame.time.Clock()
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                board.dest = 0, 0
-                board.rot = 0
-                board.get_click(event.pos)
-            if board.chosen:
-                if event.type == pygame.KEYDOWN and board.focused_cell:
-                    if event.key == pygame.K_UP or event.key == pygame.K_w:
-                        board.dest = 0, -1
-                        move_counter -= 1
-                        board.rot = 0
-                    if event.key == pygame.K_DOWN or event.key == pygame.K_s:
-                        board.dest = 0, 1
-                        move_counter -= 1
-                        board.rot = 0
-                    if event.key == pygame.K_LEFT or event.key == pygame.K_a:
-                        board.dest = 0, 0
-                        board.rot = 90
-                    if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
-                        board.dest = 0, 0
-                        board.rot = -90
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_SPACE:
-                        move_counter -= 1
-                        board.shoot()
-        screen.fill((0, 0, 0))
-        pygame.draw.rect(screen, "#d55800", (
-            board.left, board.top, board.cell_size * board.width, board.cell_size * board.height))  # подложка
+class Game:
+    def main(mapp):
+        pygame.init()
+        sht_snd = pygame.mixer.Sound('data/vystrel-tanka.mp3')
+        pygame.display.set_caption('Игра')
+        size = width, height = 1920 * 3 / 4, 1080 * 3 / 4
+        screen = pygame.display.set_mode(size)
+        board = MainBoard(11, 11, 100, 10, 70, mapp)
+        board.set_view(300, 10, 70)
         board.first_render(screen)
-        board.map_sp.draw(screen)  # создание карты
-        board.render(screen)
-        board.extra_sp.draw(screen)
-        if move_counter == 0:
-            board.player = (board.player + 1) % 2
-            move_counter = default_mc
-            board.chosen = 0
-            board.focused.kill()
-            if last_chosen:
-                tmp_chosen = last_chosen
-                last_chosen = board.focused_cell
-                board.on_click(tmp_chosen)
-            else:
-                last_chosen = board.focused_cell
-        board.endgame()
-        if ticks >= speed:
-            if time_on:
-                board.next_move()
-            ticks = 0
-        pygame.display.flip()
-        clock.tick(60)
-        ticks += 1
-    pygame.quit()
+        os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+        running = True
+        time_on = False
+        speed = 15
+        ticks = 0
+        last_chosen = None
+        default_mc = 3 # кол-во ходов по условию
+        move_counter = default_mc
+        board.player = 0
+        clock = pygame.time.Clock()
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                    board.dest = 0, 0
+                    board.rot = 0
+                    board.get_click(event.pos)
+                if board.chosen:
+                    if event.type == pygame.KEYDOWN and board.focused_cell:
+                        if event.key == pygame.K_UP or event.key == pygame.K_w:
+                            board.dest = 0, -1
+                            move_counter -= 1
+                            board.rot = 0
+                        if event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                            board.dest = 0, 1
+                            move_counter -= 1
+                            board.rot = 0
+                        if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                            board.dest = 0, 0
+                            board.rot = 90
+                        if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                            board.dest = 0, 0
+                            board.rot = -90
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_SPACE:
+                            move_counter -= 1
+                            sht_snd.play()
+                            board.shoot()
+            screen.fill((0, 0, 0))
+            pygame.draw.rect(screen, "#d55800", (
+                board.left, board.top, board.cell_size * board.width, board.cell_size * board.height))  # подложка
+            board.first_render(screen)
+            board.map_sp.draw(screen)  # создание карты
+            board.render(screen)
+            board.extra_sp.draw(screen)
+            if move_counter == 0:
+                board.player = (board.player + 1) % 2
+                move_counter = default_mc
+                board.chosen = 0
+                board.focused.kill()
+                if last_chosen:
+                    tmp_chosen = last_chosen
+                    last_chosen = board.focused_cell
+                    board.on_click(tmp_chosen)
+                else:
+                    last_chosen = board.focused_cell
+            board.endgame()
+            if ticks >= speed:
+                if time_on:
+                    board.next_move()
+                ticks = 0
+            pygame.display.flip()
+            clock.tick(60)
+            ticks += 1
+        pygame.quit()
 
 
 if __name__ == "__main__":
     all_sprites = pygame.sprite.Group()
-    main() #выбор, что запускать, песочницу или игру main/sandbox
+    Game.main() #выбор, что запускать, песочницу или игру main/sandbox
