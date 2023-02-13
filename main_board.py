b50ee551e967f3e25f6b169db81cf6a0e572adb6
AgoS01 <tolyanchekaif@gmail.com>
2023-02-13 23:28:27 +0300

    меняю скорость пули

--- main_board.py
+++ main_board.py
@@ -28,7 +28,7 @@ class MainBoard(Board):
             self.bull_dest = 0, 0
             self.bullet_ex = 0
             self.player = 0
-            self.bullet_speed = 30
+            self.bullet_speed = 50
             self.rot_mas = {'7':180,'8':0,'9':270,'10':90,'5':270,'6':90}
 
     def player_move(self, cell):
